54278ac2be587959f4d88a8824a2e52f9ad66d75
Emily <a2017110728@estudiantes.upsa.edu.bo>
2021-03-01 16:11:05 -0400

    Practica Productos de alimentos (correcta), prueba en Postman (correcta)

---
+++ Producto/Practica1-3-21/Models/Producto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Practica1_3_21.Models
+{
+    public class Producto
+    {
+        public int codigo { get; set; }
+        
+        public string nombre { get; set; }
+
+        public double precio { get; set; }
+        public string moneda { get; set; }
+
+        public string foto { get; set; }
+    }
+}
